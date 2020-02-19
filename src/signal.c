6b5ca29a877c2bd322ef149657db3d50ffbef9fd
Thomas Jouanolle <tjouanolle@localhost.localdomain>
2020-02-19 15:33:21 +0100

    Add finished signal.c

--- src/signal.c
+++ src/signal.c
@@ -16,7 +16,24 @@ void answer_player_two(int sig, siginfo_t *siginfo, UNUSED void *context)
     }
 }
 
-void handle_signal(int sig, siginfo_t *siginfo, UNUSED void *context)
+void handle_result_signal(int step)
+{
+    if (step == 2 || step == 0) {
+        if (sig == SIGUSR1)
+            SIGNAL[2] = 1;
+        else if(sig == SIGUSR2)
+            SIGNAL[2] = 2;
+        step++;
+    } else if (step == 3 || step == 1) {
+        if (sig == SIGUSR1)
+            SIGNAL[2] *= 3;
+        else if(sig == SIGUSR2)
+            SIGNAL[2] *= 4;
+        step = (step + 1) % 4;
+    }
+}
+
+void handle_signal_player2(int sig, siginfo_t *siginfo, UNUSED void *context)
 {
     static int step = 0;
 
@@ -24,7 +41,28 @@ void handle_signal(int sig, siginfo_t *siginfo, UNUSED void *context)
         step = 1;
     else if (step == 1 && sig == SIGUSR1) {
         step = 2;
-        SIGNAL[2] = 1;
+        SIGNAL[2] = -1;
+        return;
+    }
+    if (step == 2 || step == 3)
+        handle_result_signal(step);
+    if (sig == SIGUSR1)
+        SIGNAL[0]++;
+    else if (sig == SIGUSR2)
+        SIGNAL[1]++;
+}
+
+void handle_signal_player1(int sig, siginfo_t *siginfo, UNUSED void *context)
+{
+    static int step = 0;
+
+    if (step == 0 || step == 1)
+        handle_result_signal(step);
+    if (step == 2 && sig == SIGUSR2)
+        step = 3;
+    else if (step == 3 && sig == SIGUSR1) {
+        step = 0;
+        SIGNAL[2] = -1;
         return;
     }
     if (sig == SIGUSR1)
@@ -37,11 +75,13 @@ void set_sigaction(int player)
 {
     struct sigaction act;
 
-    act.sa_sigaction = &handle_signal;
+    if (player == 1)
+        act.sa_sigaction = &handle_signal_player1;
+    else if (player == 2)
+        act.sa_sigaction = &handle_signal_player2;
     act.sa_flags = SA_SIGINFO;
     sigaction(SIGUSR1, &act, NULL);
-    if (player == 0) {
+    if (player == 0)
         act.sa_sigaction = &answer_player_two;
-    }
     sigaction(SIGUSR2, &act, NULL);
 }
